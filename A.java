ef577764cbf72c1afe83d85ce802c44329f4606b
Jody7 <jody7777PVP@gmail.com>
2016-01-05 17:32:25 -0800

    Update A.java

--- A.java
+++ A.java
@@ -1,22 +1,24 @@
 public class A {
 
+    //DEBUGEE
+
     //-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=7777
 
     static int somthing;
     static String word;
 
     public static void main(String[] args){
-        int localthing = 0;
+        int localthing = 2;
 
         B b = new B();
 
         while(true) {
-            localthing++;
+            localthing = localthing * 2;
             b.inc();
 
             somthing++;
             word = word + "lol";
-            System.out.println(somthing);
+            //System.out.println(somthing);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
